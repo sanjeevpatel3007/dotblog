ac0198279c98fe0c27fbf0ba22048cf82e870abd
Sanjeev Patel <153625737+sanjeevpatel3007@users.noreply.github.com>
2024-09-27 18:56:20 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -14,7 +14,9 @@ export const sendToken = (user, statusCode, message, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
+    httpOnly: false,
+   sameSite: 'none',
+   secure:true 
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
