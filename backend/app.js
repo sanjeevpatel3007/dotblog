c82fe8a6c2a519adba1299ed30bbe2cf1e22531c
Sanjeev Patel <153625737+sanjeevpatel3007@users.noreply.github.com>
2024-09-27 18:09:16 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dotblog-frontend.onrender.com",
     methods: ["GET", "PUT", "DELETE", "POST"],
     credentials: true,
   })
