79e4fbbea87ec133151e44c8f443aa4acfd7df25
Sanjeev Patel <153625737+sanjeevpatel3007@users.noreply.github.com>
2024-09-27 17:54:40 +0530

    Update MyBlogs.jsx

--- frontend/src/components/miniComponents/MyBlogs.jsx
+++ frontend/src/components/miniComponents/MyBlogs.jsx
@@ -10,7 +10,7 @@ const MyBlogs = () => {
   useEffect(() => {
     const fetchMyBlogs = async () => {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/blog/myblogs",
+        "https://blog-backend-xmbi.onrender.com/api/v1/blog/myblogs",
         { withCredentials: true }
       );
       setMyBlogs(data.blogs);
@@ -20,7 +20,7 @@ const MyBlogs = () => {
 
   const deleteBlogHandler = async (id) => {
     await axios
-      .delete(`http://localhost:4000/api/v1/blog/delete/${id}`, {
+      .delete(`https://blog-backend-xmbi.onrender.com/api/v1/blog/delete/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
